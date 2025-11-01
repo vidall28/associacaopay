2cd0102fea415229b584abe430d2186dc873b9ac
vidalkaique <dosreiskaiquevidal@gmail.com>
2025-11-01 12:25:41 -0300

    corrigindo erros

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "@/react-app/pages/Home";
 import AdminPage from "@/react-app/pages/Admin";
 
